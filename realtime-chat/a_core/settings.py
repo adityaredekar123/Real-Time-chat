9c6f020f47fff702b79de5b755f28369e1ddf8bc
ASR-010203 <redekaraditya1234@gmail.com>
2024-09-22 12:41:41 +0530

    commit

--- realtime-chat/a_core/settings.py
+++ realtime-chat/a_core/settings.py
@@ -15,10 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-rj#-z^kx3j+1ay397otg6j8m_8#v^$^$jys6&41vy^&6le)ezc'
 
